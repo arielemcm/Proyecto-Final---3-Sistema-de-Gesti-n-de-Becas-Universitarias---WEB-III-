22697d406381cfb6604e7c37faabb1f848a69375
Jose Luis Mayta Chura <joseluismaytachura@gmail.com>
2026-06-09 19:49:34 -0400

    Añadir vista de login y configurar ruta raíz

--- SistemaGestBecasUniv/BecasUniv/views.py
+++ SistemaGestBecasUniv/BecasUniv/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('admin:index')
+        messages.error(request, 'Usuario o contraseña inválidos')
+
+    return render(request, 'login.html')
